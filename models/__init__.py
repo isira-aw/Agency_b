f4c890023275a941f9428e0e9a32546d4ce253b8
Isira Adithya <isira.weerasinghe@arimaclanka.com>
2025-11-19 23:16:33 +0530

    tested done

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,6 @@ from .user import User
 from .booking import Booking
 from .gallery import GalleryImage
 from .document import UserDocument
-from .calendar_event import CalendarEvent
 from .settings import Settings
 
-__all__ = ["User", "Booking", "GalleryImage", "UserDocument", "CalendarEvent", "Settings"]
+__all__ = ["User", "Booking", "GalleryImage", "UserDocument", "Settings"]
